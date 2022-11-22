ae5f7b1604e885cffe4d935996272b52bb11a62a
isabellagarci24 <113448979+isabellagarcia1@users.noreply.github.com>
2022-11-22 15:37:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world")
+"""
+this code written un python 3.8
+print(50)
+print (3.5)
+print (50,"letters",3.10)
+print("hello",end = "")
+print (50)
+print (3.10)
+print (50,"letters",3.10)
+print ("hello", end ="")
+print ("world")
